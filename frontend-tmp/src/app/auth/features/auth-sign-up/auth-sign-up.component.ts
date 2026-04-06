730b649a606f5477a524794a254e7ab152d1ec65
andres.mera <feli041089@gmail.com>
2026-04-06 14:51:23 -0500

    feat(front): Arreglando el git del front.

---
+++ frontend-tmp/src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-sign-up',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      auth-sign-up works!
+    </p>
+  `,
+  styleUrl: './auth-sign-up.component.css'
+})
+export default class AuthSignUpComponent {
+
+}
