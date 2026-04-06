93179f9db192a0cb4bf7e27f262c86049b996e81
andres.mera <feli041089@gmail.com>
2026-04-06 10:08:56 -0500

    Project creation.

---
+++ app-login-back/src/main/java/com/project/app_login_back/AppLoginBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.app_login_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLoginBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLoginBackApplication.class, args);
+	}
+
+}
