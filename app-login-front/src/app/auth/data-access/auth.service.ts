3d03be5595f8cd9dbcbcdd71dc866ff2ce645475
andres.mera <feli041089@gmail.com>
2026-04-25 16:59:10 -0500

    feat(front): Modification to consume new endpoint.

--- app-login-front/src/app/auth/data-access/auth.service.ts
+++ app-login-front/src/app/auth/data-access/auth.service.ts
@@ -7,13 +7,13 @@ import { Login } from '../interface/login.interface';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private http = inject(HttpClient);
-  private baseUrl = '/api/v1/logintoken';
+  private baseUrl = '/api/v1/login';
 
   signUp(user: User): Observable<User> {
     return this.http.post<User>(`${this.baseUrl}/signup`, user);
   }
 
   login(credentials: string): Observable<Login> {
-    return this.http.post<any>(`${this.baseUrl}`, credentials);
+    return this.http.post<any>(`${this.baseUrl}/in`, credentials);
   }
 }
