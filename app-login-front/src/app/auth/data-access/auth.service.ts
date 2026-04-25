c12920e14e8c6a351f8e753712375b65f4091bed
andres.mera <feli041089@gmail.com>
2026-04-25 15:18:45 -0500

    feat(front): Creation of the Login operation services.

---
+++ app-login-front/src/app/auth/data-access/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user.interface';
+import { Login } from '../interface/login.interface';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private http = inject(HttpClient);
+  private baseUrl = '/api/v1/logintoken';
+
+  signUp(user: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/signup`, user);
+  }
+
+  login(credentials: string): Observable<Login> {
+    return this.http.post<any>(`${this.baseUrl}`, credentials);
+  }
+}
--- app-login-front/src/app/auth/features/auth-log-in/auth-log-in.component.ts
+++ app-login-front/src/app/auth/features/auth-log-in/auth-log-in.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../data-access/auth.service';
+
+interface LoginForm {
+  email: FormControl<null | string>;
+  password: FormControl<null | string>;
+}
 
 @Component({
   selector: 'app-auth-log-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth-log-in.component.html',
   styleUrl: './auth-log-in.component.css'
 })
 export default class AuthLogInComponent {
+  private _formBuilder = inject(FormBuilder);
+  private _authService = inject(AuthService);
 
+  public form: FormGroup = this._formBuilder.group<LoginForm>({
+    email: this._formBuilder.control(null, [Validators.required, Validators.email]),
+    password: this._formBuilder.control(null, [Validators.required]),
+  });
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    console.log(this.form.value);
+    this._authService.login(this.form.value).subscribe({
+      next: (response) => {
+        // 1. Lo que pasa cuando el login es EXITOSO
+        console.log('¡Bienvenido!', response);
+        // Aquí podrías guardar el token y navegar a la plataforma
+        // this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        // 2. Lo que pasa si el backend responde con ERROR (401, 500, etc.)
+        console.error('Error en el login:', err);
+        // Aquí puedes mostrar un mensaje: "Usuario o contraseña incorrectos"
+      },
+      complete: () => {
+        // 3. (Opcional) Se ejecuta cuando termina el flujo, pase lo que pase
+        console.log('Proceso de login finalizado');
+      }
+    });
+
+  }
 }
